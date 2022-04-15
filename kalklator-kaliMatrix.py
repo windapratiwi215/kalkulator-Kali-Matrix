6dd6e634aa54ae3723ab0dd05768c9635c089523
Rosalina Winda Pratiwi <103246042+windapratiwi215@users.noreply.github.com>
2022-04-15 11:45:52 +0700

    modifikasi file

--- kalklator-kaliMatrix.py
+++ kalklator-kaliMatrix.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Apr 15 09:55:44 2022
 
-@author: Ngga tau namanya
+@author: Rosalina Winda Pratiwi
 """
 
 # -*- coding: utf-8 -*-
@@ -45,109 +45,103 @@ label_col2.grid(row=6, column=0, sticky="nesw")
 input_col2 = Entry(window, font=("Times New Roman", 10))
 input_col2.grid(row=6, column=2, sticky="news" )
 def bentuk_matrix():
-    win1 = Tk()
-    win1.title("Kalikan Matrix")
-    win1.geometry("400x400+120+120")
-    win1.configure(bg='#49A')
-    win1.resizable(False, False)
-            
-    #make matiks A
-    text_var = []
-    entries = []
-    temp_mat = []
-    def get_mat():
-        matrix = []
-        for i in range(rows):
-            matrix.append([])
-            for j in range(cols):
-                matrix[i].append(text_var[i][j].get())
+   window1 = Tk()
+   window1.title("Perkalian 2 Buah Matrik")
+   window1.geometry("400x400+120+120")
+   window1.configure(bg='#49A')
+   window1.resizable(False, False)
 
-        print(matrix)
-        matrixB = []
-        for i in range(rows_B):
-            matrixB.append([])
-            for j in range(cols_B):
-                matrixB[i].append(text_varB[i][j].get())
-        print(matrixB)
-        temp_row = []
+   #make matiks A
+   text_var = []
+   entries = []
+   temp_mat = []
+   def get_mat():
+       matrix = []
+       for i in range(rows):
+           matrix.append([])
+           for j in range(cols):
+               matrix[i].append(text_var[i][j].get())
 
-        temp_sum = 0
-     
-        for i in range(0, row_matriks(matrix)):
-            for j in range(0, col_matriks(matrixB)):
-                for k in range(0, row_matriks(matrixB)):
-                    temp_sum += matrix[i][k] * matrixB[k][j]
-                
-                temp_row.append(temp_sum)
-                temp_sum = 0
-            temp_mat.append(temp_row)
-            temp_row = []
-        print(temp_mat)
-        ##print matriks hasil 
-        Label(win1, text="Hasil Perkalian Matrik A dan B :", font=('arial', 10, 'bold'), 
-          bg='#0059b3').place(x=20, y=210)
-        Label(win1, text=temp_mat, font=('arial', 10, 'bold'), 
-          bg='white').place(x=20, y=240)
-          
-    def col_matriks(matriks):
-        return len(matriks[0])
-     
-    def row_matriks(matriks):
-        return len(matriks) 
+       print(matrix)
+       matrixB = []
+       for i in range(rows_B):
+           matrixB.append([])
+           for j in range(cols_B):
+               matrixB[i].append(text_varB[i][j].get())
+       print(matrixB)
+       temp_row = []
 
-    Label(win1, text="Matrik A :", font=('arial', 10, 'bold'), 
-          bg="#0059b3").place(x=20, y=20)
+       temp_sum = 0
+    
+       for i in range(0, row_matriks(matrix)):
+           for j in range(0, col_matriks(matrixB)):
+               for k in range(0, row_matriks(matrixB)):
+                   temp_sum += matrix[i][k] * matrixB[k][j]
+               
+               temp_row.append(temp_sum)
+               temp_sum = 0
+           temp_mat.append(temp_row)
+           temp_row = []
+       print(temp_mat)
+       ##print matriks hasil 
+       Label(window1, text="Hasil Perkalian Matrik A dan B :", font=('arial', 10, 'bold'), 
+         bg='#0059b3').place(x=20, y=210)
+       Label(window1, text=temp_mat, font=('arial', 10, 'bold'), 
+         bg='white').place(x=20, y=240)
+         
+   def col_matriks(matriks):
+       return len(matriks[0])
+    
+   def row_matriks(matriks):
+       return len(matriks) 
 
-    row_mat1 = int(input_row1.get())
-    col_mat1 = int(input_col1.get())
-    x2=0
-    y2=0
-    rows, cols = (row_mat1,col_mat1)
-    for i in range(rows):
-        # append an empty list to your two arrays
-        # so you can append to those later
-        text_var.append([])
-        entries.append([])
-        for j in range(cols):
-            # append your IntVar and Entry
-            text_var[i].append(IntVar())
-            entries[i].append(Entry(win1, textvariable=text_var[i][j].get(),width=3))
-            entries[i][j].place(x=20 + x2, y=50 + y2)
-            x2 += 30
-        y2 += 30
-        x2 = 0    
-    #make matriks B
-    Label(win1, text="Matrik B :", font=('arial', 10, 'bold'), 
-          bg="#0059b3").place(x=200, y=20)
+   Label(window1, text="Matrik A :", font=('arial', 10, 'bold'), 
+         bg="#0059b3").place(x=20, y=20)
 
-    text_varB = []
-    entriesB = []
-    row_mat2 = int(input_row2.get())
-    col_mat2 = int(input_col2.get())
-    x3 = 0
-    y3 = 0
-    rows_B, cols_B = (row_mat2,col_mat2)
-    for k in range(rows_B):
-        # append an empty list to your two arrays
-        # so you can append to those later
-        text_varB.append([])
-        entriesB.append([])
-        for l in range(cols_B):
-            # append your IntVar and Entry
-            text_varB[k].append(IntVar())
-            entriesB[k].append(Entry(win1, textvariable=text_varB[k][l],width=3))
-            entriesB[k][l].place(x=200 + x3, y=50 + y3)
-            x3 += 30
+   x2 = 0
+   y2 = 0
+   rows, cols = (int(input_row1.get()),int(input_col1.get()))
+   for i in range(rows):
+       # append an empty list to your two arrays
+       # so you can append to those later
+       text_var.append([])
+       entries.append([])
+       for j in range(cols):
+           # append your IntVar and Entry
+           text_var[i].append(IntVar())
+           entries[i].append(Entry(window1, textvariable=text_var[i][j],width=3))
+           entries[i][j].place(x=20 + x2, y=50 + y2)
+           x2 += 30
+       y2 += 30
+       x2 = 0    
+   #make matriks B
+   Label(window1, text="Matrik B :", font=('arial', 10, 'bold'), 
+         bg="#0059b3").place(x=200, y=20)
 
-        y3 += 30
-        x3 = 0    
-    button= Button(win1,text="Hitung", bg='#0059b3', width=15, command=get_mat)
-    button.place(x=200,y=300)      
-          
+   text_varB = []
+   entriesB = []
+   x3 = 0
+   y3 = 0
+   rows_B, cols_B = (int(input_row2.get()),int(input_col2.get()))
+   for k in range(rows_B):
+       # append an empty list to your two arrays
+       # so you can append to those later
+       text_varB.append([])
+       entriesB.append([])
+       for l in range(cols_B):
+           # append your IntVar and Entry
+           text_varB[k].append(IntVar())
+           entriesB[k].append(Entry(window1, textvariable=text_varB[k][l],width=3))
+           entriesB[k][l].place(x=200 + x3, y=50 + y3)
+           x3 += 30
+
+       y3 += 30
+       x3 = 0    
+   button= Button(window1,text="Hitung", bg='#0059b3', width=15, command=get_mat)
+   button.place(x=200,y=300)     
+   
                    
 buttonBentukMatrix= Button(window, text="Submit", font=("Times New Roman", 10), command=bentuk_matrix)
 buttonBentukMatrix.grid(row=9, column=3, sticky="nesw")
-
-
-                  
+              
 window.mainloop()
